345cc67876430e1d360bc5c025e80704c9e29ede
Dave Turner <davetii@gmail.com>
2018-11-17 14:24:58 -0500

    Initial hello node express project. basic templates, helpers, rest calls

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('greet', (arg) => {
+    return 'Hello ' + arg.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send({name: 'dave', age: 'old', wt:220 });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/home', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'About Page',
+        welcomeText: 'I am the welcome text'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({code: 232, errorMessage: 'shit didnt work'});
+});
+
+app.listen(3000, () => {
+    console.log('server is up')
+});
