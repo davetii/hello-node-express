dbaa83f77ab37af1b11591c2cd84ba96052ae707
Dave Turner <davetii@gmail.com>
2018-11-17 14:48:00 -0500

    Basic middleware samples: using the use method to inject before the request with and without the next call

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
-
+let underMaintenance = true;
 app.set('view engine', 'hbs');
+
+if (underMaintenance) {
+    app.use((req, res) => {
+        res.render('maintenance.hbs', {
+            pageTitle: 'maintenance Page'
+        });
+    });
+}
+
 app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partials');
+
+app.use((req, resp,next) => {
+    let now = new Date().toString();
+    let log  = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+       if(err) {
+           console.log('error during logging');
+       }
+    });
+    next();
+});
+
+
+
+
+
+
+
 hbs.registerHelper('currentYear', () => {
     return new Date().getFullYear();
 });
